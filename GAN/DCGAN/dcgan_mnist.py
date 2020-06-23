041d1700aee106160b15c21ab0fd570385340ec9
ConorLazarou <conor.lazarou@7shifts.com>
2020-06-23 16:40:31 -0600

    added mnist dcgan

---
+++ GAN/DCGAN/dcgan_mnist.py
@@ -0,0 +1,291 @@
+import os
+
+import torch
+from torch import nn
+import torch.optim as optim
+import torchvision as tv
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim=100, batchnorm=True):
+        """A generator for mapping a latent space to a sample space.
+
+        The sample space for this generator is single-channel, 28x28 images
+        with pixel intensity ranging from -1 to +1.
+
+        Args:
+            latent_dim (int): latent dimension ("noise vector")
+            batchnorm (bool): Whether or not to use batch normalization
+        """
+        super(Generator, self).__init__()
+        self.latent_dim = latent_dim
+        self.batchnorm = batchnorm
+        self._init_layers()
+
+    def _init_layers(self):
+        """Initialize the layers."""
+        # Project the input
+        self.linear1 = nn.Linear(self.latent_dim, 7*7*256, bias=False)
+        self.bn1d1 = nn.BatchNorm1d(7*7*256) if self.batchnorm else None
+        self.leaky_relu = nn.LeakyReLU()
+
+        # Convolutions
+        self.conv1 = nn.Conv2d(
+                in_channels=256,
+                out_channels=128,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+                bias=False)
+        self.bn2d1 = nn.BatchNorm2d(128) if self.batchnorm else None
+
+        self.conv2 = nn.ConvTranspose2d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False)
+        self.bn2d2 = nn.BatchNorm2d(64) if self.batchnorm else None
+
+        self.conv3 = nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=1,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False)
+        self.tanh = nn.Tanh()
+
+
+    def forward(self, input_tensor):
+        """Forward pass; map latent vectors to samples."""
+        intermediate = self.linear1(input_tensor)
+        intermediate = self.bn1d1(intermediate)
+        intermediate = self.leaky_relu(intermediate)
+
+        intermediate = intermediate.view((-1, 256, 7, 7))
+
+        intermediate = self.conv1(intermediate)
+        if self.batchnorm:
+            intermediate = self.bn2d1(intermediate)
+        intermediate = self.leaky_relu(intermediate)
+
+        intermediate = self.conv2(intermediate)
+        if self.batchnorm:
+            intermediate = self.bn2d2(intermediate)
+        intermediate = self.leaky_relu(intermediate)
+
+        intermediate = self.conv3(intermediate)
+        output_tensor = self.tanh(intermediate)
+        return output_tensor
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        """A discriminator for discerning real from generated images.
+
+        Images must be single-channel and 28x28 pixels.
+
+        params:
+            input_dim (int): width of the input
+            layers (List[int]): A list of layer widths including output width
+
+        Output activation is Sigmoid.
+        """
+        super(Discriminator, self).__init__()
+        self._init_layers()  # I know this is overly-organized. Fight me.
+
+    def _init_layers(self):
+        """Initialize the layers."""
+        # Convolutions
+        self.conv1 = nn.Conv2d(
+                in_channels=1,
+                out_channels=64,
+                kernel_size=5,
+                stride=2,
+                padding=2,
+                bias=True)
+        self.leaky_relu = nn.LeakyReLU()
+        self.dropout_2d = nn.Dropout2d(0.3)
+
+        self.conv2 = nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=5,
+                stride=2,
+                padding=2,
+                bias=True)
+
+        self.linear1 = nn.Linear(128*7*7, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        """Forward pass; map samples to confidence they are real [0, 1]."""
+        intermediate = self.conv1(input_tensor)
+        intermediate = self.leaky_relu(intermediate)
+        intermediate = self.dropout_2d(intermediate)
+
+        intermediate = self.conv2(intermediate)
+        intermediate = self.leaky_relu(intermediate)
+        intermediate = self.dropout_2d(intermediate)
+
+        intermediate = intermediate.view((-1, 128*7*7))
+        intermediate = self.linear1(intermediate)
+        output_tensor = self.sigmoid(intermediate)
+
+        return output_tensor
+
+
+
+
+class DCGAN():
+    def __init__(self, latent_dim, noise_fn, dataloader,
+                 batch_size=32, device='cpu', lr_d=1e-3, lr_g=2e-4):
+        """A very basic DCGAN class for generating MNIST digits
+
+        Args:
+            generator: a Ganerator network
+            discriminator: A Discriminator network
+            noise_fn: function f(num: int) -> pytorch tensor, (latent vectors)
+            dataloader: a pytorch dataloader for loading images
+            batch_size: training batch size. Must match that of dataloader
+            device: cpu or CUDA
+            lr_d: learning rate for the discriminator
+            lr_g: learning rate for the generator
+        """
+        self.generator = Generator(latent_dim).to(device)
+        self.discriminator = Discriminator().to(device)
+        self.noise_fn = noise_fn
+        self.dataloader = dataloader
+        self.batch_size = batch_size
+        self.device = device
+        self.criterion = nn.BCELoss()
+        self.optim_d = optim.Adam(self.discriminator.parameters(),
+                                  lr=lr_d, betas=(0.5, 0.999))
+        self.optim_g = optim.Adam(self.generator.parameters(),
+                                  lr=lr_g, betas=(0.5, 0.999))
+        self.target_ones = torch.ones((batch_size, 1), device=device)
+        self.target_zeros = torch.zeros((batch_size, 1), device=device)
+
+    def generate_samples(self, latent_vec=None, num=None):
+        """Sample images from the generator.
+
+        Images are returned as a 4D tensor of values between -1 and 1.
+        Dimensions are (number, channels, height, width). Returns the tensor
+        on cpu.
+
+        Args:
+            latent_vec: A pytorch latent vector or None
+            num: The number of samples to generate if latent_vec is None
+
+        If latent_vec and num are None then use self.batch_size
+        random latent vectors.
+        """
+        num = self.batch_size if num is None else num
+        latent_vec = self.noise_fn(num) if latent_vec is None else latent_vec
+        with torch.no_grad():
+            samples = self.generator(latent_vec)
+        samples = samples.cpu()  # move images to cpu
+        return samples
+
+    def train_step_generator(self):
+        """Train the generator one step and return the loss."""
+        self.generator.zero_grad()
+
+        latent_vec = self.noise_fn(self.batch_size)
+        generated = self.generator(latent_vec)
+        classifications = self.discriminator(generated)
+        loss = self.criterion(classifications, self.target_ones)
+        loss.backward()
+        self.optim_g.step()
+        return loss.item()
+
+    def train_step_discriminator(self, real_samples):
+        """Train the discriminator one step and return the losses."""
+        self.discriminator.zero_grad()
+
+        # real samples
+        pred_real = self.discriminator(real_samples)
+        loss_real = self.criterion(pred_real, self.target_ones)
+
+        # generated samples
+        latent_vec = self.noise_fn(self.batch_size)
+        with torch.no_grad():
+            fake_samples = self.generator(latent_vec)
+        pred_fake = self.discriminator(fake_samples)
+        loss_fake = self.criterion(pred_fake, self.target_zeros)
+
+        # combine
+        loss = (loss_real + loss_fake) / 2
+        loss.backward()
+        self.optim_d.step()
+        return loss_real.item(), loss_fake.item()
+
+    def train_epoch(self, print_frequency=10, max_steps=0):
+        """Train both networks for one epoch and return the losses.
+
+        Args:
+            print_frequency (int): print stats every `print_frequency` steps.
+            max_steps (int): End epoch after `max_steps` steps, or set to 0
+                             to do the full epoch.
+        """
+        loss_g_running, loss_d_real_running, loss_d_fake_running = 0, 0, 0
+        for batch, (real_samples, _) in enumerate(self.dataloader):
+            real_samples = real_samples.to(self.device)
+            ldr_, ldf_ = self.train_step_discriminator(real_samples)
+            loss_d_real_running += ldr_
+            loss_d_fake_running += ldf_
+            loss_g_running += self.train_step_generator()
+            if print_frequency and (batch+1) % print_frequency == 0:
+                print(f"{batch+1}/{len(self.dataloader)}:"
+                      f" G={loss_g_running / (batch+1):.3f},"
+                      f" Dr={loss_d_real_running / (batch+1):.3f},"
+                      f" Df={loss_d_fake_running / (batch+1):.3f}",
+                      end='\r',
+                      flush=True)
+            if max_steps and batch == max_steps:
+                break
+        if print_frequency:
+            print()
+        loss_g_running /= batch
+        loss_d_real_running /= batch
+        loss_d_fake_running /= batch
+        return (loss_g_running, (loss_d_real_running, loss_d_fake_running))
+
+
+def main():
+    import matplotlib.pyplot as plt
+    from time import time
+    batch_size = 32
+    epochs = 100
+    latent_dim = 100
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    transform = tv.transforms.Compose([
+            tv.transforms.Grayscale(num_output_channels=1),
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize((0.5,), (0.5,))
+            ])
+    dataset = ImageFolder(
+            root=os.path.join("data", "mnist_png", "training"),
+            transform=transform
+            )
+    dataloader = DataLoader(dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2
+            )
+    noise_fn = lambda x: torch.rand((x, latent_dim), device=device)
+    gan = DCGAN(latent_dim, noise_fn, dataloader, device=device)
+    for i in range(10):
+        gan.train_epoch()
+    images = gan.generate_samples() * -1
+    ims = tv.utils.make_grid(images, normalize=True)
+    plt.imshow(ims.numpy().transpose((1,2,0)))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
